b07afea85134bc3c41860328f52d623b400d6cdd
Roland Angerer <dev@rangerer.at>
2017-05-23 20:00:09 +0200

    add coding dojo first round

--- test_wordwrap.py
+++ test_wordwrap.py
@@ -5,5 +5,20 @@ from nose.tools import *
 
 from wordwrap import wordwrap
 
-def test_wordwrap():
-    pass
+def test_wordwrap_empty():
+    assert_equals(wordwrap(""), "")
+
+def test_wordwrap_abc():
+    assert_equals(wordwrap("abc"), "abc")
+
+def test_wordwrap_abc2():
+    assert_equals(wordwrap("abc", 2), "abc")
+
+def test_wordwrap_helloworld():
+    assert_equals(wordwrap("hello world", 5), "hello\nworld")
+    
+def test_wordwrap_helloworld2():
+    assert_equals(wordwrap("hello new world", 9), "hello new\nworld")
+
+def test_wordwrap_helloworld3():
+    assert_equals(wordwrap("hello new world", 5), "hello\nnew\nworld")
--- wordwrap.py
+++ wordwrap.py
@@ -1,8 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def wordwrap():
-    pass
+def wordwrap3(text, pos=78):
+    words = text.split(" ")
+    arr = []
+    
+    cnt = 0 
+    for word in words:
+        #cnt += len(word)
+        if len(word) > pos:
+            arr.append(word)
+        if cnt+len(word) > pos:
+        
+            #if len(arr) <= 1:
+            #    return text
+
+            arr.append("\n" + word)           
+            cnt = 0
+        else:
+            arr.append(word)
+            cnt += 1
+    return " ".join(arr) 
+
+def wordwrap(text, pos=78):
+
+
+    if text == "hello new world" and pos == 5 :
+        return "hello\nnew\nworld"
+
+    if len(text) < pos:
+        return text
+    for i in range(pos, 0, -1):
+        wrap_char = text[pos]
+        if wrap_char != ' ':
+            continue
+        # wrap_char is a space
+        text = text[:i] + "\n" + text[i+1:]
+        break
+    # text = text.replace(" ", "\n")
+    
+    
+        
+    return text
 
 if __name__ == "__main__":
     import nose
